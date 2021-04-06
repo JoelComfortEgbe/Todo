1531f373da48ce1d128d320f41873256844c4b85
JoelComfortEgbe <46760565+JoelComfortEgbe@users.noreply.github.com>
2021-04-06 23:43:04 +0100

    task added

---
+++ js/todo.js
@@ -0,0 +1,76 @@
+    function get_todos() {
+        var todos = new Array;
+        var todos_str = localStorage.getItem('todo');
+        if (todos_str !== null) {
+            todos = JSON.parse(todos_str); 
+        }
+        return todos;
+    }
+     
+    function add() {
+        var task = document.getElementById('task').value;
+        if (task == "") {
+            alert("please enter a valid task")
+        }else{
+            var todos = get_todos();
+            todos.push(task);
+            localStorage.setItem('todo', JSON.stringify(todos));
+         
+            show();
+         
+            return false;
+        }
+     
+        
+    }
+     
+    function remove() {
+        var id = this.getAttribute('id');
+        var todos = get_todos();
+        todos.splice(id, 1);
+        localStorage.setItem('todo', JSON.stringify(todos));
+     
+        show();
+     
+        return false;
+    }
+     
+    function search(){
+        let search_todo = document.getElementById('search').value
+        input = input.toLowerCase();
+        let x = document.getElementsByClassName('todo')
+        for (i = 0; i < x.length; i++){
+            if (!x[i].innerHTML.toLowerCase().includes(input)) {
+                x[i].style.display="none";
+            } 
+            else {
+                x[i].style.display="list-item";                 
+            } 
+        }    
+    }
+
+    function show() {
+        var todos = get_todos();
+        if (todos.length < 1) {
+            var html = "No Task added";
+            document.getElementById('todos').innerHTML = html;
+        }else{ 
+            var html = '<ul>';
+            for(var i=0; i<todos.length; i++) {
+                html += '<li>' + todos[i] + '<button class="remove" id="' + i  + '">Delete</button></li>';
+            };
+            html += '</ul>';
+         
+            document.getElementById('todos').innerHTML = html;
+         
+            var buttons = document.getElementsByClassName('remove');
+            for (var i=0; i < buttons.length; i++) {
+                buttons[i].addEventListener('click', remove);
+            };
+        }
+     
+        
+    }
+     
+    document.getElementById('add').addEventListener('click', add);
+    show();
